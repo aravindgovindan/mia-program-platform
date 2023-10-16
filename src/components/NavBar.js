d5a80e0b2a229891e65219e0630dc01bb12a1209
Aravind Govindan <aravindgovindan1@gmail.com>
2023-10-16 12:27:09 -0700

    Modify checkbox label and title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,6 @@ function NavBar({ navItems }) {
   }
 
   const toggleIcons = () => {
-    console.log(showIcons)
     setShowIcons(!showIcons)
   }
   return (
@@ -25,7 +24,10 @@ function NavBar({ navItems }) {
           <div className="pointer grow ph1 f3" onClick={toggleOpen}><GiHamburgerMenu /></div>
         </div>
         <div className={`sidenav ${open ? 'open flex flex-column ph2' : 'dn'}`}>
-          <div className="pv2 ph1 f5"><input type='checkbox' checked={showIcons} onChange={toggleIcons}></input> <span>Show Icons</span></div>
+          <div className="pv2 ph1 f5" title="Uncheck to hide navbar completely on collapse">
+            <input type='checkbox' checked={showIcons} onChange={toggleIcons}></input>
+            <span className="pointer" onClick={toggleIcons}> Show Icons on collapse</span>
+          </div>
           {(navItems || []).map(item => <NavItem key={item.label} label={item.label} icon={item.icon} />)}
         </div>
       </> : <>
@@ -34,8 +36,11 @@ function NavBar({ navItems }) {
             {open && <div className="f3 b pl3">Mia</div>}
             <div className="pointer grow ph1 f3" onClick={toggleOpen}><GiHamburgerMenu/></div>
           </div>
-          <div className="ph2 flex flex-column">
-            {open && <div className="pv2 ph1 f5"><input type='checkbox' checked={showIcons} onChange={toggleIcons}></input> <span>Show Icons</span></div>}
+          <div className="ph2 flex flex-column" title="Uncheck to hide navbar completely on collapse">
+            {open && <div className="pv2 ph1 f5">
+              <input type='checkbox' checked={showIcons} onChange={toggleIcons}></input>
+              <span className="pointer" onClick={toggleIcons}> Show Icons on collapse</span>
+            </div>}
             {(navItems || []).map(item => <NavItem key={item.label} label={item.label} icon={item.icon} collapsed={!open} />)}
           </div>
         </div>
