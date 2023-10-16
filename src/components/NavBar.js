a321efb31fad11b37af8894af39c1018d67b9b0e
Aravind Govindan <aravindgovindan1@gmail.com>
2023-10-16 12:26:43 -0700

    Fix hamburger icon not registering clicks sometimes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,9 @@ function NavBar({ navItems }) {
   const [open, setOpen] = useState(true);
   const [showIcons, setShowIcons] = useState(true);
 
-  const toggleOpen = () => {
+  const toggleOpen = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
     setOpen(!open);
   }
 
@@ -20,7 +22,7 @@ function NavBar({ navItems }) {
       {!showIcons ? <>
         <div className={`flex justify-between pa2 ${open ? '' : 'absolute'}`} style={{ backgroundColor: "rgba(10, 48, 120, 0.2)" }}>
           {open && <div className="f3 b pl3">Mia</div>}
-          <div className="pointer grow ph1 f3"><GiHamburgerMenu onClick={toggleOpen} /></div>
+          <div className="pointer grow ph1 f3" onClick={toggleOpen}><GiHamburgerMenu /></div>
         </div>
         <div className={`sidenav ${open ? 'open flex flex-column ph2' : 'dn'}`}>
           <div className="pv2 ph1 f5"><input type='checkbox' checked={showIcons} onChange={toggleIcons}></input> <span>Show Icons</span></div>
@@ -30,7 +32,7 @@ function NavBar({ navItems }) {
         <div className={`sidenav ${open ? 'open' : ''}`}>
           <div className="flex justify-between pa2" style={{ backgroundColor: "rgba(10, 48, 120, 0.2)" }}>
             {open && <div className="f3 b pl3">Mia</div>}
-            <div className="pointer grow ph1 f3"><GiHamburgerMenu onClick={toggleOpen} /></div>
+            <div className="pointer grow ph1 f3" onClick={toggleOpen}><GiHamburgerMenu/></div>
           </div>
           <div className="ph2 flex flex-column">
             {open && <div className="pv2 ph1 f5"><input type='checkbox' checked={showIcons} onChange={toggleIcons}></input> <span>Show Icons</span></div>}
