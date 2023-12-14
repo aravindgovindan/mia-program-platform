979574b746f8e538a95cb87007143b955e222ab2
Aravind Govindan <aravindgovindan1@gmail.com>
2023-12-13 19:28:44 -0700

    make node labels editable with a dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import Icon from './Icon';
+
+function Dropdown({ value, options, onSelect, placeholder }) {
+
+  const [selected, setSelected] = useState((options.find(option => option.id === value) || { id: '' }).id);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {setSelected((options.find(option => option.id === value)|| { id: '' }).id)}, [value])
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  }
+
+  const selectOption = (option) => {
+    setSelected(option);
+    setOpen(false);
+    onSelect(option);
+  }
+
+  return (
+    <div className="flex relative items-center pv1">
+      <div
+        className={`flex items-center justify-between ba ph2 pv1 relative pointer bw1 b--light-accent`}
+        onClick={toggleOpen}
+      >
+        {selected.length ? <div className="flex-grow-1 pv1">{options.find(option => option.id === selected).label}</div>
+          : <div className="gray flex-grow-1 pv1">{placeholder || 'Select...'}</div>
+        }
+        <Icon icon={'caretDown'} className="flex items-center pv1 ml3 f4" />
+        <div 
+          className={`absolute overflow-y-auto top-2 ba b--light-accent bw1 mt1 w-100 left-0 bg-white z-999 flex-column ${open ? 'flex' : 'dn'}`}
+          style={{maxHeight: '100px'}}
+        >
+          {
+            options.map(option => (
+              <DropdownOption key={option.id} id={option.id} label={option.label} onSelect={selectOption} />
+            ))
+          }
+        </div>
+      </div>
+
+    </div>
+  )
+
+}
+
+
+function DropdownOption({ id, label, onSelect }) {
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    onSelect(id);
+  }
+
+  return (
+    <div
+      className="pv2 bb b--moon-gray ph2 flex items-center hover-bg-gray"
+      onClick={handleClick}
+    >
+      {label}
+    </div>
+  )
+
+}
+export default Dropdown;
--- src/components/NodeItem.js
+++ src/components/NodeItem.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiFillEdit, AiOutlineDelete } from 'react-icons/ai';
+import Dropdown from './Dropdown';
 
 function NodeItem({ nodeName, isSelected }) {
 
   const borderStyle = isSelected ? 'b--blue bw2 br3' : 'b--moon-gray br2'
+  const gradeOptions = [
+    {id: 'Grade K', label: 'Grade K'},
+    {id: 'Grade 1', label: 'Grade 1'},
+    {id: 'Grade 2', label: 'Grade 2'},
+    {id: 'Grade 3', label: 'Grade 3'},
+    {id: 'Grade 4', label: 'Grade 4'},
+    {id: 'Grade 5', label: 'Grade 5'},
+
+  ]
+  const [node, setNode] = useState(nodeName)
+  const [editing, setEditing] = useState(false)
+
+  const handleEditClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setEditing(true);
+  }
+
+  const onDropdownSelect = (selectedOption) => {
+    setNode(selectedOption);
+    setEditing(false);
+  }
 
   return (
     <div className={`ba pa3 bg-white mb2 hide-child ${borderStyle}`}>
       <div className='f5 pb2 flex justify-between'>
-        <div>{nodeName}</div>
+        {!editing
+          ? <div>{node}</div>
+          : <Dropdown options={gradeOptions} value={node} onSelect={onDropdownSelect} />
+        }
         <div>
-          <button className='child bn bg-white'><AiFillEdit /></button>
+          <button className='child bn bg-white' onClick={handleEditClick}><AiFillEdit /></button>
           <button className='child bn bg-white'><AiOutlineDelete /></button>
         </div>
       </div>
